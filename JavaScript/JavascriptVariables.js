0de3057576b5d3909f97f23374ce74f69115c995
Yash Bhandari <133970746+bhandariyash76@users.noreply.github.com>
2026-02-04 17:01:31 +0530

    added js variables

---
+++ JavaScript/JavascriptVariables.js
@@ -0,0 +1,16 @@
+// we can declare caribles using 3 keywords var,let and cosnt
+
+var message = "hi geeks"
+console.log(message)
+message = "rewirtten var"
+console.log(message)
+
+let text = "hello geeks"
+console.log(text)
+text = "rewritten text"
+console.log(text)
+
+const year = 2004
+console.log(year)
+year = 3
+console.log(year)
